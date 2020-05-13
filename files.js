4a70e720e2c2fa88bb602ab9d9a3663ec4292a14
kriyatuo <64539318+kriyatuo@users.noreply.github.com>
2020-05-13 10:58:04 +0000

    apllication du 13/05/2020

---
+++ files.js
@@ -0,0 +1,76 @@
+{ 
+    "Name":"Schema",
+    "field":[
+	{
+        "name":"HDI_rank",
+        "type":"integer",
+        "description":"Indice de Developpement Humain."
+    },
+	{
+        "name":"Country",
+        "type":"string",
+        "description":"nom de l'un des 177 pays ici mentionné."
+	},
+	{
+        "name":"Gender-related development index Rank",
+        "type":"integer",
+        "description":"rang de l'Indice de développement lié au relation de genre"
+        },
+	{
+        "name":"Gender-related development index Value",
+        "type":"number",
+        "description":"valeur de l'Indice de développement lié au relation de genre."
+	},
+	{
+        "name":"Life expectancy at birth (years) 2005 Female",
+        "type":"number",
+        "description":"Espérance de vie à la naissance des femmes (années) 2005"
+	},
+	{
+        "name":"Life expectancy at birth (years) 2005 Male",
+        "type":"number",
+        "decription":"Espérance de vie à la naissance des hommes (années) 2005"
+	},
+	{
+        "name":"Adult literacy ratea (% aged 15 and older) 1995-2005 Female",
+        "type":"number",
+        "description":"Ratio d'alphabétisation des adultes femmes (% 15 ans et plus) 1995-2005"
+	},
+	{
+        "name":"Adult literacy ratea (% aged 15 and older) 1995-2005 Male",
+        "type":"number",
+        "description":"Ratio d'alphabétisation des adultes hommes (% 15 ans et plus) 1995-2005"
+	},
+	{
+        "name":"Combined gross enrolment ratio for primary, secondary and tertiary education (%) 2005 Female",
+        "type":"integer",
+        "description":"Taux brut de scolarisation combiné pour l'enseignement primaire, secondaire et supérieur (%) 2005 Femmes"
+	},
+	{
+        "name":"Combined gross enrolment ratio for primary, secondary and tertiary education (%) 2005 Male",
+        "type":"integer",
+        "description":"Taux brut de scolarisation combiné pour l'enseignement primaire, secondaire et supérieur (%) 2005 Hommes"
+	},
+	{
+        "name":"Estimated earned incomec (PPP US$) 2005 Female",
+        "type":"number",
+        "description":"Estimation des revenus gagnés (PPA US $) 2005 Femmes"
+	},
+	{
+        "name":"Estimated earned incomec (PPP US$) 2005 Male",
+        "type":"number",
+        "description":"Estimation des revenus gagnés (PPA US $) 2005 Hommes"
+    },
+    {
+        "name":"HDI rank minus GDI rank",
+        "type":"integer",
+        "description":"Classement HDI moins classement GDI"
+    },
+    {
+        "name":"development level",
+        "type":"string",
+        "description":"Niveau de developpement"
+    }
+	],
+    "missingValues"; ["NaN", "..", " "]
+}
